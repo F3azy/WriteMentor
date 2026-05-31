d6dd51ba04530c257479ec379edc33ae4e717b8c
Feazy <fcarvelli.work@gmail.com>
2026-05-31 13:44:00 +0200

    lang picker pointer to cursor on hover

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -1,11 +1,16 @@
 const LanguagePicker = () => {
   return (
-    <div className="px-3 py-4 
+    <div
+      className="px-3 py-4 
     flex justify-center items-center
     gap-x-2
     text-white font-bold
-    border-white border-2 rounded-lg">
-      <img width={28} src="https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1f1ec-1f1e7.svg" /> 
+    border-white border-2 rounded-lg hover:cursor-pointer"
+    >
+      <img
+        width={28}
+        src="https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1f1ec-1f1e7.svg"
+      />
       English {">"}
     </div>
   );
