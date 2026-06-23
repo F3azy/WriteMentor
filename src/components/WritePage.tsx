79c12b9d38d51d77c46347531d068c8b2eaef5ce
Feazy <fcarvelli.work@gmail.com>
2026-06-23 22:05:17 +0200

    write area design

--- src/components/WritePage.tsx
+++ src/components/WritePage.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import LanguagePicker from "./LanguagePicker";
 import Button from "./Button";
 
+const temp =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.";
+
 export default function WritePage() {
   const [visible, setVisible] = useState(false);
 
@@ -15,7 +18,7 @@ export default function WritePage() {
   return (
     <div
       className={`
-        h-full flex flex-col gap-y-8
+        flex flex-1 flex-col gap-y-8
         transition-all duration-500 ease-out
         ${visible ? "opacity-100 translate-x-0" : "opacity-0 translate-x-6"}
       `}
@@ -25,10 +28,13 @@ export default function WritePage() {
         <Button className="tracking-wider">Change</Button>
       </div>
 
-      <textarea
-        className="flex-1 w-full p-4 border rounded-lg resize-none"
-        placeholder="Start writing..."
-      />
+      <div className="relative flex-1 bg-secondary rounded-lg font-bold text-2xl text-black">
+        <p className="absolute inset-0 p-4 opacity-30">{temp}</p>
+        <textarea
+          name="Write"
+          className="absolute inset-0 p-4 rounded-lg resize-none border-0 focus:outline-none bg-transparent"
+        />
+      </div>
     </div>
   );
 }
